ae334b4f2a796b65bd0779989b733688e1383148
Blue-Onion <rawatadityasingh963@gmail.com>
2025-11-28 11:29:47 +0530

    Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <main>page</main>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
---
+++ components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { navIcons, navLinks } from '@/data'
+import dayjs from 'dayjs'
+import Image from 'next/image'
+
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <nav>
+                <div className="">
+                    <Image
+                    height={15}
+                    width={15}
+                    alt='logo'
+                        src='/images/logo.svg'
+                    />
+                    <p className="font-bold">
+                        Aditya
+                    </p>
+                <ul>
+                    {
+                        navLinks.map((item)=>{
+                            return <li key={item.id}>
+                                    {item.name}
+                            </li>
+                        })
+                    }
+                </ul>
+                </div>
+                <div className="">
+                    <ul>
+                        {navIcons.map(({id,img})=>{
+                         return   <li key={id}>
+                                <Image
+                                height={15}
+                                width={15}
+                                alt='logo'
+                                src={img}
+                                />
+                            </li>
+                        })}
+                    </ul>
+                    <time >{dayjs().format('ddd MMM D h:mm A')}</time>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
